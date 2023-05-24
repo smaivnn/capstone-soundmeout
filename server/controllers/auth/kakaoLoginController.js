b5394ab93ec4e2652d556e04e0308ae714d5ad50
smaivnn <janus321@naver.com>
2023-05-24 21:34:08 +0900

    feat : 토큰 발급 함수, google OAuth2 적용

--- server/controllers/auth/kakaoLoginController.js
+++ server/controllers/auth/kakaoLoginController.js
@@ -1,13 +1,12 @@
 const axios = require("axios");
 const User = require("../../model/User");
+const { IssueToken } = require("./issueToken");
 
 const handleKakaoLogin = async (req, res) => {
   const CODE = req.body.code;
-  // const ID = "62905cdd843de0ba5b4606b6cd28b12b";
-  const ID = "2fb671d206c0c16d72ebd001177c140a";
-  // const redirect = "https://www.soundmeout.net/auth/kakao/oAuth";
-  const redirect = "http://localhost:3000/auth/OAuth/kakao";
-  const getTokenURL = `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${ID}&redirect_uri=${redirect}&code=${CODE}`;
+  const ID = process.env.KAKAO_ID;
+  const REDIRECT_URI = process.env.KAKAO_REDIRECT_URI;
+  const getTokenURL = `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${ID}&redirect_uri=${REDIRECT_URI}&code=${CODE}`;
   const getUserInfoURL = `https://kapi.kakao.com//v2/user/me`;
 
   try {
@@ -51,12 +50,13 @@ const handleKakaoLogin = async (req, res) => {
         });
       }
 
+      const tokens = await IssueToken(foundUser);
       //유저가 있다면 refreshToken을 갱신해준다.
-      foundUser.refreshToken = refreshToken;
+      foundUser.refreshToken = tokens.refreshToken;
       foundUser.save();
 
       // 사용하기 위해서는 front에서 withCredential : true를 주어야 한다.
-      res.cookie("refreshToken", refreshToken, {
+      res.cookie("refreshToken", tokens.refreshToken, {
         httpOnly: true,
         sameSite: "None",
         maxAge: 24 * 60 * 60 * 1000, // 86400초(하루)
@@ -67,17 +67,11 @@ const handleKakaoLogin = async (req, res) => {
         status: 200,
         success: true,
         message: "성공적인 로그인",
-        ACCESS_TOKEN: accessToken,
-        userObject: {
-          lognId: foundUser.loginId,
-          name: foundUser.name,
-          email: foundUser.email,
-          roles: foundUser.roles,
-        },
-      }); // response항목에 맞게 return하기
+        accessToken: tokens.accessToken,
+      });
     }
   } catch (error) {
-    // accessToken 만료일때 에러 설정하기.
+    // accessToken 만료일때 에러 설정하기. > middleware로 변경
   }
 };
 
