cfb62b2a680cf178e7fa15703517176f9279c2d9
smaivnn <janus321@naver.com>
2023-05-26 19:28:23 +0900

    feat : google oAuth2 클아이언트 code 전송 방식으로 변경

---
+++ server/controllers/auth/LegacyGoogleLoginController.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ * 
+ *  이 controller는 더이상 쓰이지 않으나
+ *  추후 리팩토링 과정에서 쓰일 수 있기에
+ *  남겨놓음.
+ * 
+ */
+
+
+const handleGoogleLogin = async (req, res) => {
+  let url = `https://accounts.google.com/o/oauth2/v2/auth`;
+  url += `?client_id=${process.env.GOOGLE_CLIENT_ID}`;
+  url += `&redirect_uri=${process.env.GOOGLE_REDIRECT_URI}`;
+  url += `&response_type=code`;
+  url += `&scope=email profile`;
+
+  return res.redirect(url);
+};
+
+module.exports = { handleGoogleLogin };
