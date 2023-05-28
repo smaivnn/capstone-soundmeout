1a6c03b066faa154bfda9009f0f42401ce0ed618
smaivnn <janus321@naver.com>
2023-05-28 19:43:49 +0900

    feat : for make other branch

--- server/controllers/auth/LegacyGoogleLoginController.js
+++ server/controllers/auth/LegacyGoogleLoginController.js
@@ -1,12 +1,11 @@
 /**
- * 
- * 
+ *
+ *
  *  이 controller는 더이상 쓰이지 않으나
  *  추후 리팩토링 과정에서 쓰일 수 있기에
  *  남겨놓음.
- * 
+ *
  */
-
 
 const handleGoogleLogin = async (req, res) => {
   let url = `https://accounts.google.com/o/oauth2/v2/auth`;
