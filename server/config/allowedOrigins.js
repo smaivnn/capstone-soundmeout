ea98f60594de3018a73e151528e1f7d10b519dd4
usablechan <lch8910@naver.com>
2023-08-02 17:59:46 +0900

    test : request ip 확인용

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,4 +1,5 @@
 const allowedOrigins = [
+  "https://www.soundmeout.net",
   "http://www.soundmeout.net",
   "http://127.0.0.1:5500",
   "http://localhost:3500",
--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const credentials = require("./utils/middleware/credentials");
 const cookieParser = require("cookie-parser");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.js");
-
+const requestIp = require("request-ip");
 const connectDB = require("./config/dbConn");
 
 const PORT = process.env.PORT || 3500;
@@ -37,5 +37,9 @@ app.use("/paper", require("./routes/paper"));
 app.use("/noti", require("./routes/noti"));
 app.use("/follow", require("./routes/follow"));
 app.use("/user", require("./routes/user"));
+app.use("/", (req, res) => {
+  conosol.log("ip", requestIp.getClientIp(req));
+  return res.status(200).json({ message: "Welcome to SoundMeOut API!" });
+});
 
 app.listen(PORT, () => logger.info(`Server running on port ${PORT}`));
